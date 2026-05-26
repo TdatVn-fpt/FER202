303fac1c7e5ff00817364ea2da9ae97b74649629
Dat Lai Lap Trinh <ntiendat2108@gmail.com>
2026-05-26 15:13:13 +0700

    test

---
+++ Module1_JS_ES6/0_practice.js
@@ -0,0 +1,13 @@
+// tập luyện lưu ý file này không gợi ý cách viết syntax, chỉ là bài tập thực hành
+// Bài 1
+const age = 21;
+const name = "Đạt";
+// dấu `` gọi là backticks hoặc template literals, nó cho phép chúng ta chèn biến vào trong chuỗi một cách dễ dàng hơn
+console.log(`Tôi tên là ${name} và tôi ${age} tuổi`);
+
+// arrow function
+// cú pháp const temHam = (thamso) => {}
+const sumNumber1 = (a,b) => a+b; 
+console.log(`Kết quả của phép tính a + b là:  ${5+7}`);
+
+
